aef2f30043bcc681572a803f0aed9594e3b1d6b3
Jennifer <jen.yiu.90@gmail.com>
2019-02-24 15:37:46 -0800

    complete purchase item test

--- __tests__/vending_machine.spec.js
+++ __tests__/vending_machine.spec.js
@@ -2,18 +2,10 @@ const VendingMachine = require('../src/vending-machine');
 
 describe('Vending Machine Sandwiches 🥪', () => {
   // let vendingMachine, inventory, coinCase;
-  let test;
+  let machine;
 
   beforeEach(() => {
-    test = {};
-
-    test.coinCase = {
-      toonie: { amount: 100, value: 2 },
-      loonie: { amount: 100, value: 1 },
-      quarter: { amount: 100, value: 0.25 },
-      dime: { amount: 100, value: 0.1 },
-      nickle: { amount: 100, value: 0.05 }
-    };
+    // test = {};
 
     test.fullInventory = [
       {
@@ -31,13 +23,13 @@ describe('Vending Machine Sandwiches 🥪', () => {
       {
         productCode: 'A3',
         productName: 'Peanut Butter & Jelly Sandwich',
-        productCost: 4.25,
+        productCost: 3.75,
         quantity: 10
       },
       {
         productCode: 'A4',
         productName: 'BLT Sandwich',
-        productCost: 3.75,
+        productCost: 4,
         quantity: 10
       },
       {
@@ -48,98 +40,64 @@ describe('Vending Machine Sandwiches 🥪', () => {
       }
     ];
 
-    test.inventory = {
-      1: {
-        productCode: 'A1',
-        productName: 'Tuna Sandwich',
-        productCost: 3.25,
-        quantity: 5
-      },
-      2: {
-        productCode: 'A2',
-        productName: 'Egg & Sausage Sandwich',
-        productCost: 3.25,
-        quantity: 7
-      },
-      3: {
-        productCode: 'A3',
-        productName: 'Peanut Butter & Jelly Sandwich',
-        productCost: 4.25,
-        quantity: 5
-      },
-      4: {
-        productCode: 'A4',
-        productName: 'BLT Sandwich',
-        productCost: 3.75,
-        quantity: 4
-      },
-      5: {
-        productCode: 'A5',
-        productName: 'Roastbeef Sandwich',
-        productCost: 4.25,
-        quantity: 8
-      }
-    };
-
-    // test.machine = new VendingMachine(test.inventory, test.coinCase);
+    machine = new VendingMachine('../data.json');
   }); // end of beforeEach()
 
   describe('queryInventory', () => {
-    beforeEach(() => {
-      test.machine = new VendingMachine(test.inventory);
-    });
-
     describe('When inventory exists', () => {
-      it('should print entire info of item when productName is entered', () => {
-        const result = test.machine.queryInventory('Roastbeef Sandwich');
+      it('should print entire item when productName is entered', () => {
+        const result = machine.queryInventory('Roastbeef Sandwich');
         expect(result).toEqual('(A5) Roastbeef Sandwich - $4.25, qty: 8');
       });
-      it('should print entire info of item when productCode is entered', () => {
-        const result = test.machine.queryInventory('A3');
+      it('should print entire item when productCode is entered', () => {
+        const result = machine.queryInventory('A3');
         expect(result).toEqual(
-          '(A3) Peanut Butter & Jelly Sandwich - $4.25, qty: 5'
+          '(A3) Peanut Butter & Jelly Sandwich - $3.75, qty: 5'
         );
       });
-      it.skip('should print current inventory in console', () => {
-        const result = test.machine.queryInventory();
-        const print =
-          'Tuna Sandwich: Code: A1, Price: $3.25, Qty: 5, Egg & Sausage Sandwich: Code: A2, Price: $3.25, Qty: 7Peanut Butter & Jelly Sandwich: Code: A3, Price: $4.25, Qty: 5, BLT Sandwich: Code: A4, Price: $3.75, Qty: 4, Roastbeef Sandwich: Code: A5, Price: $4.25, Qty: 8';
-
-        expect(result).toBe(print);
-      });
+    });
+    it('should say item sold out', () => {
+      const result = machine.queryInventory('A4');
+      expect(result).toEqual('Sold Out');
     });
 
     describe('When inventory does not exist', () => {
-      it('should throw an error when name does not exist', () => {
-        const result = test.machine.queryInventory('gummy bears');
-        expect(result).toThrow();
-      });
-      it('should throw an error when code does not exist', () => {
-        const result = test.machine.queryInventory('D7');
-        expect(result).toThrow('Invalid Item Code');
+      it.skip('should throw an error, incorrect name', () => {
+        const result = machine.queryInventory('gummy bears');
+        expect(result).toThrow('Invalid Item');
       });
     });
+  }); //end of queryInventory()
 
-    describe.skip('Restock', () => {
-      beforeEach(() => {
-        test.machine = new VendingMachine(test.inventory);
-      });
-
-      it('should restock the quantity of each sandwich to 10', () => {
-        const result = test.machine.restockInventory();
-        expect(result).toEqual(10);
-      });
+  describe('printInventory', () => {
+    it('should print inventory in console', () => {
+      const result = machine.printInventory();
+      expect(result).toBe(true);
     });
-  }); //end of queryInventory
+  }); // end of printInventory()
 
-  describe.skip('purchase sandwich with exact change', () => {
-    beforeEach(() => {
-      test.machine = new VendingMachine(test.inventory);
+  describe('restockInventory', () => {
+    it('should restock the quantity of each sandwich to 10', () => {
+      const result = machine.restockInventory();
+      expect(result).toEqual(10);
     });
+  }); //end of restockInventory()
 
-    it('should return sandwich and decrement it by one, and return change 0', () => {
-      const result = test.machine.purchaseItem('A3', 0);
-      expect(result).toEqual('Chicken Sandwich');
+  describe('refillChange', () => {
+    it('should refill coinCase to 100 each', () => {
+      const result = machine.refillChange();
+      expect(result).toEqual(100);
+    });
+  }); //end of refillChange()
+
+  describe('Dispense Product', () => {
+    it('should dispense sandwich with no change, and reduce quantity by 1', () => {
+      const result = machine.purchaseItem('A3', 3.75);
+      expect(result).toEqual({
+        name: 'Peanut Butter & Jelly Sandwich',
+        quantity: 9,
+        change: 0
+      });
     });
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,45 +1,83 @@
-const processData = require('./process-data');
-
 class VendingMachine {
-  constructor(inventory, coinCase) {
-    if (inventory.length === 0) {
-      throw 'Empty Inventory.';
-    }
-
-    this.inventory = inventory;
-    this.coinCase = coinCase;
+  constructor(data) {
+    this.data = require(data);
+    this.slots = Object.keys(this.data.inventory);
+    // console.log('************', Object.keys(this.data.coinCase)[loonie]);
   }
 
   queryInventory(string) {
     for (let i = 1; i <= 5; i++) {
-      if (
-        this.inventory[i].productName === string ||
-        this.inventory[i].productCode === string
-      ) {
-        const sandwich = this.inventory[i];
-        return `(${sandwich.productCode}) ${sandwich.productName} - $${
-          sandwich.productCost
-        }, qty: ${sandwich.quantity}`;
-      } else if (!this.inventory[i].productName || !string) {
-        throw 'Invalid Item';
+      let item = this.data.inventory[i];
+
+      if (item.productCode === string && item.quantity === 0) {
+        return 'Sold Out';
       }
 
-      // console.log('************', this.inventory[i]);
-      // console.log('############', string);
+      if (item.productName === string || item.productCode === string) {
+        return `(${item.productCode}) ${item.productName} - $${
+          item.productCost
+        }, qty: ${item.quantity}`;
+      }
     } // end of for Loop
-  } //end of queryInventory
+  } //end of queryInventory()
+
+  printInventory() {
+    this.slots.forEach(slot => {
+      const sw = this.data.inventory[slot];
+      console.log(
+        `(${sw.productCode}) ${sw.productName} - $${sw.productCost}, qty: ${
+          sw.quantity
+        }`
+      );
+    });
+
+    return true;
+  } // end of printInventory()
 
   restockInventory() {
     let qty = 0;
-
-    this.inventory.forEach(item => {
-      if (item.quantity < 10) {
-        item.quantity = 10;
+    this.slots.forEach(slot => {
+      const sw = this.data.inventory[slot];
+      if (sw.quantity < 10) {
+        sw.quantity = 10;
       }
-      qty = 10;
+      qty = sw.quantity;
     });
     return qty;
-  }
+  } // end of restockInventory()
+
+  refillChange() {
+    const coins = Object.keys(this.data.coinCase);
+    let amt = 0;
+    // console.log('????????', Object.entries(this.data.coinCase));
+
+    coins.map(value => {
+      const coin = this.data.coinCase[value];
+
+      if (coin.amount < 100) {
+        coin.amount = 100;
+      }
+      amt = coin.amount;
+    });
+    return amt;
+  } //end of refillChange()
+
+  purchaseItem(input, money) {
+    let purchase = { name: '', quantity: null, change: null };
+
+    this.slots.forEach(slot => {
+      const sw = this.data.inventory[slot];
+
+      if (input === sw.productCode) {
+        console.log('++++++++++++', sw);
+        purchase.name = sw.productName;
+        purchase.quantity = sw.quantity -= 1;
+        purchase.change = money - sw.productCost;
+      }
+    });
+
+    return purchase;
+  } //end of purchaseItem()
 } //end of VendingMachine
 
 module.exports = VendingMachine;
