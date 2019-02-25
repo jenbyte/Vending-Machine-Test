8bd094042260334e65699b52b287343142968677
Jennifer <jen.yiu.90@gmail.com>
2019-02-24 22:38:03 -0800

    convert change to coin value in descending order

--- __tests__/vending_machine.spec.js
+++ __tests__/vending_machine.spec.js
@@ -1,6 +1,6 @@
 const VendingMachine = require('../src/vending-machine');
 
-describe('Vending Machine Sandwiches 🥪', () => {
+describe('Sandwich Vending Machine 🥪', () => {
   // let vendingMachine, inventory, coinCase;
   let machine;
 
@@ -47,34 +47,27 @@ describe('Vending Machine Sandwiches 🥪', () => {
     describe('When inventory exists', () => {
       it('should print entire item when productName is entered', () => {
         const result = machine.queryInventory('Roastbeef Sandwich');
-        expect(result).toEqual('(A5) Roastbeef Sandwich - $4.25, qty: 8');
+        expect(result).toEqual('(A5) Roastbeef Sandwich - $5.75, qty: 8');
       });
       it('should print entire item when productCode is entered', () => {
         const result = machine.queryInventory('A3');
         expect(result).toEqual(
-          '(A3) Peanut Butter & Jelly Sandwich - $3.75, qty: 5'
+          '(A3) Peanut Butter & Jelly Sandwich - $4.50, qty: 5'
         );
       });
     });
-    it('should say item sold out', () => {
-      const result = machine.queryInventory('A4');
-      expect(result).toEqual('Sold Out');
-    });
 
     describe('When inventory does not exist', () => {
+      it('should say item sold out when qty is 0', () => {
+        const result = machine.queryInventory('A4');
+        expect(result).toEqual('Sold Out');
+      });
       it.skip('should throw an error, incorrect name', () => {
         const result = machine.queryInventory('gummy bears');
         expect(result).toThrow('Invalid Item');
       });
     });
   }); //end of queryInventory()
-
-  describe('printInventory', () => {
-    it('should print inventory in console', () => {
-      const result = machine.printInventory();
-      expect(result).toBe(true);
-    });
-  }); // end of printInventory()
 
   describe('restockInventory', () => {
     it('should restock the quantity of each sandwich to 10', () => {
@@ -90,14 +83,35 @@ describe('Vending Machine Sandwiches 🥪', () => {
     });
   }); //end of refillChange()
 
+  describe('printInventory', () => {
+    it('should print inventory in console', () => {
+      const result = machine.printInventory();
+      expect(result).toBe(true);
+    });
+  }); // end of printInventory()
+
   describe('Dispense Product', () => {
     it('should dispense sandwich with no change, and reduce quantity by 1', () => {
-      const result = machine.purchaseItem('A3', 3.75);
-      expect(result).toEqual({
-        name: 'Peanut Butter & Jelly Sandwich',
-        quantity: 9,
-        change: 0
-      });
+      const result = machine.purchaseItem('A3', 4.5);
+      expect(result).toEqual(
+        'Name: Peanut Butter & Jelly Sandwich, Change: $0.00 = $2*(0), $1*(0), 25¢*(0), 10¢*(0), 5¢*(0), VMQty: 9'
+      );
+    });
+    it('should return correct change and reduce quantity by 1', () => {
+      const result = machine.purchaseItem('A2', 5);
+      expect(result).toEqual(
+        'Name: Egg & Sausage Sandwich, Change: $0.50 = $2*(0), $1*(0), 25¢*(2), 10¢*(0), 5¢*(0), VMQty: 9'
+      );
+    });
+    it('should return money if less than price', () => {
+      const result = machine.purchaseItem('A3', 4.25);
+      expect(result).toEqual('Insufficient Funds');
+    });
+    it('should return money correct change in descending order', () => {
+      const result = machine.purchaseItem('A1', 6.65);
+      expect(result).toBe(
+        'Name: Tuna Sandwich, Change: $2.40 = $2*(1), $1*(0), 25¢*(1), 10¢*(1), 5¢*(0), VMQty: 9'
+      );
     });
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,37 +2,23 @@ class VendingMachine {
   constructor(data) {
     this.data = require(data);
     this.slots = Object.keys(this.data.inventory);
-    // console.log('************', Object.keys(this.data.coinCase)[loonie]);
   }
 
   queryInventory(string) {
     for (let i = 1; i <= 5; i++) {
       let item = this.data.inventory[i];
-
       if (item.productCode === string && item.quantity === 0) {
         return 'Sold Out';
       }
 
-      if (item.productName === string || item.productCode === string) {
-        return `(${item.productCode}) ${item.productName} - $${
-          item.productCost
-        }, qty: ${item.quantity}`;
+      if (item.productName === string || item.productCode === string || '') {
+        let price = item.productCost.toFixed(2);
+        return `(${item.productCode}) ${item.productName} - $${price}, qty: ${
+          item.quantity
+        }`;
       }
     } // end of for Loop
   } //end of queryInventory()
-
-  printInventory() {
-    this.slots.forEach(slot => {
-      const sw = this.data.inventory[slot];
-      console.log(
-        `(${sw.productCode}) ${sw.productName} - $${sw.productCost}, qty: ${
-          sw.quantity
-        }`
-      );
-    });
-
-    return true;
-  } // end of printInventory()
 
   restockInventory() {
     let qty = 0;
@@ -47,12 +33,10 @@ class VendingMachine {
   } // end of restockInventory()
 
   refillChange() {
-    const coins = Object.keys(this.data.coinCase);
+    let coins = Object.keys(this.data.coinCase);
     let amt = 0;
-    // console.log('????????', Object.entries(this.data.coinCase));
-
     coins.map(value => {
-      const coin = this.data.coinCase[value];
+      let coin = this.data.coinCase[value];
 
       if (coin.amount < 100) {
         coin.amount = 100;
@@ -62,21 +46,50 @@ class VendingMachine {
     return amt;
   } //end of refillChange()
 
-  purchaseItem(input, money) {
-    let purchase = { name: '', quantity: null, change: null };
-
+  printInventory() {
     this.slots.forEach(slot => {
       const sw = this.data.inventory[slot];
+      const printInventory = `(${sw.productCode}) ${sw.productName}-$${
+        sw.productCost
+      }, qty: ${sw.quantity}`;
+    });
+    return true;
+  } // end of printInventory()
 
-      if (input === sw.productCode) {
-        console.log('++++++++++++', sw);
+  purchaseItem(input, money) {
+    let purchase = { Change: null, Name: '', VMQty: null };
+
+    this.slots.forEach(slot => {
+      let sw = this.data.inventory[slot];
+      let change = money - sw.productCost;
+
+      if (input === sw.productCode && money >= sw.productCost) {
         purchase.name = sw.productName;
-        purchase.quantity = sw.quantity -= 1;
-        purchase.change = money - sw.productCost;
+        purchase.vmQty = sw.quantity -= 1;
+        purchase.change = change.toFixed(2);
       }
     });
 
-    return purchase;
+    let cents = purchase.change;
+    let toonie = cents / 2 >= 1 ? Math.floor(cents / 2) : 0;
+    cents -= toonie * 2;
+    let loonie = cents >= 1 ? Math.floor(cents / 1) : 0;
+    cents -= loonie;
+    let quarter = cents / 0.25 >= 1 ? Math.floor(cents / 0.25) : 0;
+    cents -= quarter * 0.25;
+    let dime = cents / 0.1 >= 1 ? Math.floor(cents / 0.1) : 0;
+    cents -= dime * 0.1;
+    let nickle = cents / 0.05 >= 1 ? Math.floor(cents / 0.05) : 0;
+    cents = Math.round(cents * 100) / 100;
+    cents -= nickle * 0.05;
+
+    return purchase.change != null
+      ? `Name: ${purchase.name}, Change: $${
+          purchase.change
+        } = $2*(${toonie}), $1*(${loonie}), 25¢*(${quarter}), 10¢*(${dime}), 5¢*(${nickle}), VMQty: ${
+          purchase.vmQty
+        }`
+      : 'Insufficient Funds';
   } //end of purchaseItem()
 } //end of VendingMachine
 
